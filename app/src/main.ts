f9c6cd00d89bc34adafc4d40ac3dfcaab416f33e
Hadi T <hadi@syniol.com>
2026-02-14 23:47:51 +0000

    Included Logger

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,12 +3,17 @@ import helmet from 'helmet'
 
 import { AppModule } from './app.module'
 import { ValidationPipe } from './libs/validation'
+import { ConsoleLogger } from '@nestjs/common'
 
 
 (async () => {
   const app = await NestFactory.create(AppModule)
   app.use(helmet())
   app.enableShutdownHooks();
+  app.useLogger(new ConsoleLogger({
+    json: true,
+    context: 'identity-provider-service',
+  }))
 
   /**
    * This is to avoid having to import and instantiate ValidationPipe in each rout
